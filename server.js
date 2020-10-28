fc3a1a9b613fa58719a4a08bda78a27ad21132e9
yulque <double.y.r@gmail.com>
2020-10-28 18:07:09 +0900

    validation fix

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ function onHttpStart() {
 
 app.set("views", __dirname + "/views");
 app.engine("handlebars", exphbs());
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
 app.set("view engine", "handlebars");
 
 //  static resources
@@ -27,28 +28,32 @@ app.use(express.static("static"));
 app.get("/", (req, res) => {
   res.status(200).render("home", data.recipe);
 });
+//login-----------------------------------------------------------------------------
 app.get("/logIn", (req, res) => {
   if (req.method == "GET") res.status(200).render("login");
 });
-
+//login validation-------------------------------------------------------------------
 app.post("/login", (req, res) => {
-  let id = req.body.userId;
-  let pw = req.body.userPassword;
+  const { userId, userPassword } = req.body;
   let result = {};
+  let validation = false;
   //check id
-  if (id == "") {
+  if (userId == "") {
     result.msgId = "Enter your id";
-    result.userName = id;
-  } else result.userName = id;
-
+    result.userName = userId;
+  } else result.userName = userId;
   //check password
-  if (pw == "") {
-    console.log("password phase");
+  if (userPassword == "") {
     result.msgPwd = "Enter your password";
-    result.userPassword = pw;
-  } else result.userPassword = pw;
-  console.log(result);
-  res.status(200).render("login", result);
+    result.userPassword = userPassword;
+  } else {
+    result.userPassword = userPassword;
+    validation = true;
+  }
+  //validation check
+  if (validation) {
+    res.render("home");
+  } else res.status(200).render("login", result);
 });
 //signUp -----------------------------------------------------------------------------
 app.get("/signUp", (req, res) => {
@@ -63,7 +68,14 @@ app.post("/signUp", (req, res) => {
   result.lastName = lastName;
   result.email = email;
   result.password = password;
-
+  //check first name
+  if (!firstName) {
+    result.msgFN = "Enter your first name";
+  }
+  //check last name
+  if (!lastName) {
+    result.msgLN = "Enter your last name";
+  }
   //check email
   if (!email) {
     result.msgId = "Enter an email address";
@@ -72,7 +84,7 @@ app.post("/signUp", (req, res) => {
     result.msgId = "Please enter a valid email";
   } else if (email.match(/(?=.*["%$+])/)) {
     result.msgId = 'Email must not contain "%$+';
-  } else validation = true;
+  }
 
   //check password
   if (!password) {
@@ -88,6 +100,7 @@ app.post("/signUp", (req, res) => {
       "Password must contain 1 digit, upper case, special character";
   } else validation = true;
 
+  //if valid information, send message
   if (validation) {
     const sgMail = require("@sendgrid/mail");
     sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
@@ -95,66 +108,32 @@ app.post("/signUp", (req, res) => {
     const msg = {
       to: "yuriyoonkim@gmail.com",
       from: "yryoon@myseneca.ca",
-      subject: "contact us form submission",
-      html: `Welcome ${firstName} ${lastName} !<br>
-            `,
+      subject: `Welcome to Friendly Veggies! ${firstName} ${lastName}`,
+      html: `Welcome ${firstName} ${lastName}!<br>
+            Please enjoy our tasty and healthy meals`,
     };
     // Asyncronously sends the email
-
     sgMail
       .send(msg)
       .then(() => {
-        res.redirect("/");
+        res.redirect("/welcome");
       })
       .catch((err) => {
-        console.log(`Error ${err}`);
-
+        console.log(`Error : ${err}`);
         res.render("signUp");
       });
-    res.status(200).render("welcome", result);
-    // for mailing ----------------------------------------------------
-    let myMail = "yuriyoonkim@gmail.com";
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: myMail,
-        pass: "db0024fl",
-      },
-    });
-
-    let mailOptions = {
-      from: myMail,
-      to: email,
-      subject: "Welcome to Friendly Veggies!",
-      text: "Please enjoy our eco-friendly and tasty meals",
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
   } else {
-    res.status(200).render("signUp", result);
+    res.render("signUp", result);
   }
 });
 // on the menu page -----------------------------------------------------
 app.get("/onTheMenu", (req, res) => {
   res.status(200).render("onTheMenu", data.recipe);
 });
-
-//-----------------------------------------------------------------------
-// const productModel = require("./static/productList");
-
-// app.get("/product/list", (req, res) => {
-//   res.render("productList", {
-//     title: "Product Listing Page",
-//     products: productModel,
-//   });
-// });
-
+// welcome dashboard ---------------------------------------------------
+app.get("/welcome", (req, res) => {
+  res.status(200).render("welcome");
+});
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
 });
